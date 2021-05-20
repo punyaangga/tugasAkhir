4998a264d51d7e599a61a7f99a85598f58d3ccf4
punya_angga <anggafantiya@gmail.com>
2021-05-20 13:30:47 +0700

    comit kedua

--- application/controllers/Validate.php
+++ application/controllers/Validate.php
@@ -49,32 +49,32 @@ class Validate extends CI_Controller{
 					'token'=>hash('sha256', $tokenTOTP));
 			$proses=$this->Validate_model->token($data);
 
-			// $dataUser = $this->db->query("SELECT * FROM user where id=$idUser limit 1")->result();        
-	  //       foreach ($dataUser as $du)
-	  //       {            
-	  //       	//send token to user
-			// 	$userkey = '0ee0ac0645f1';
-			// 	$passkey = '29d431a69be16fb2350b7186';
-			// 	$telepon = $du->telp;
-			// 	$message = 'Kode Token Kamu Adalah : '.$tokenTOTP;
-			// 	$url = 'https://console.zenziva.net/wareguler/api/sendWA/';
-			// 	$curlHandle = curl_init();
-			// 	curl_setopt($curlHandle, CURLOPT_URL, $url);
-			// 	curl_setopt($curlHandle, CURLOPT_HEADER, 0);
-			// 	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
-			// 	curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
-			// 	curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
-			// 	curl_setopt($curlHandle, CURLOPT_TIMEOUT,30);
-			// 	curl_setopt($curlHandle, CURLOPT_POST, 1);
-			// 	curl_setopt($curlHandle, CURLOPT_POSTFIELDS, array(
-			// 	    'userkey' => $userkey,
-			// 	    'passkey' => $passkey,
-			// 	    'to' => $telepon,
-			// 	    'message' => $message
-			// 	));
-			// 	$results = json_decode(curl_exec($curlHandle), true);
-			// 	curl_close($curlHandle);
-	  //       }
+			$dataUser = $this->db->query("SELECT * FROM user where id=$idUser limit 1")->result();        
+	        foreach ($dataUser as $du)
+	        {            
+	        	//send token to user
+				$userkey = '0ee0ac0645f1';
+				$passkey = '29d431a69be16fb2350b7186';
+				$telepon = $du->telp;
+				$message = 'Kode Token Kamu Adalah : '.$tokenTOTP;
+				$url = 'https://console.zenziva.net/wareguler/api/sendWA/';
+				$curlHandle = curl_init();
+				curl_setopt($curlHandle, CURLOPT_URL, $url);
+				curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+				curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
+				curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
+				curl_setopt($curlHandle, CURLOPT_TIMEOUT,30);
+				curl_setopt($curlHandle, CURLOPT_POST, 1);
+				curl_setopt($curlHandle, CURLOPT_POSTFIELDS, array(
+				    'userkey' => $userkey,
+				    'passkey' => $passkey,
+				    'to' => $telepon,
+				    'message' => $message
+				));
+				$results = json_decode(curl_exec($curlHandle), true);
+				curl_close($curlHandle);
+	        }
 		
 		
 		
